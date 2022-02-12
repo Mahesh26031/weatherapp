7f3c1e91e95b380c8c2a3634623604e94ec9b7fe
mahesh <bommisetty.mahesh03@gmail.com>
2022-02-12 09:12:18 +0530

    value making null after searching

--- script.js
+++ script.js
@@ -37,12 +37,14 @@ let weather = {
 
 document.querySelector(".search button").addEventListener("click",function(){
     weather.search();
+    document.querySelector(".search-bar").value=""
     
 })
 
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
       weather.search();
+      document.querySelector(".search-bar").value=""
     }
   });
 
